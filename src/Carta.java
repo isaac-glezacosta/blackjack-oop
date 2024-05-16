a13ea085a1d04a7ded06146e8c15e27216739859
isaac-glezacosta <igonzalez69@alumnos.uaq.mx>
2024-05-16 00:54:41 -0600

    Primera mano del juego. Pequeños cambios y comentarios

--- src/Carta.java
+++ src/Carta.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Carta {
     Random rand = new Random();
-    ArrayList<Object> cartas = new ArrayList<>(Arrays.asList('A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'));
-    int numeroAleatorio;
+    private ArrayList<Object> cartas = new ArrayList<>(Arrays.asList('A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'));
+    private int numeroAleatorio;
 
     public Object sacarCarta() {
         numeroAleatorio = rand.nextInt(13);
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        // Se crean las manos del Jugador y el Cuprier
         Mano manoCuprier = new Mano();
         Mano manoJugador = new Mano();
-        Carta carta = new Carta();
 
-        manoCuprier.agregarCartaManoCuprier();
+        // Se reparten las cartas iniciales
+        for (int i = 1; i <= 2; i++) {
+            manoCuprier.agregarCartaManoCuprier();
+            manoJugador.agregarCartaManoJugador();
+        }
+
         manoCuprier.mostrarCartaManoCuprier();
-        manoJugador.agregarCartaManoJugador();
         manoJugador.mostrarManoJugador();
     }
 }
